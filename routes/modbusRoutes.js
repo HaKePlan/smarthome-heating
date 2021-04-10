824e41e73cfe69aaa584e226761fc1a93bf78571
HaKePlan <severin.clauss@gmail.com>
2021-04-10 15:49:55 +0200

    fixing id route; adding domain route

--- routes/modbusRoutes.js
+++ routes/modbusRoutes.js
@@ -7,10 +7,11 @@ const router = express.Router();
 router.route('/').get(modbusController.getAllEntrys);
 
 router
-  .route('/:id')
+  .route('/id/:id')
   .get(modbusController.getEntryByID)
   .patch(modbusController.updateEntryByID);
 
+router.route('/domain/:domain').get(modbusController.getDomainEntrys);
 router.route('/getUpdate').get(modbusController.getAllEntrysUpdatet);
 router.route('/interval').get(modbusController.updateInterval);
 router.route('/home').get(modbusController.getHomeEntrys);
