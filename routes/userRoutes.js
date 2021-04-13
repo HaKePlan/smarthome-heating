89871250645d569147436ac79e08ddd6293f71fd
HaKePlan <severin.clauss@gmail.com>
2021-04-14 00:25:02 +0200

    added all routes for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,31 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+// AUTHROUTES
 router.post('/login', authController.login);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
+// USERROUTES
+// protect all routes after this line
+router.use(authController.protect);
+router
+  .route('/me')
+  .get(userController.getMe, userController.getUser)
+  .patch(userController.updateMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+
+// ADMINROUTES
+// restrict to admin, all routes after
+router.use(authController.restrictTo('admin', 'sudoer'));
 router
   .route('/')
-  .post(userController.createUser)
-  .get(userController.getAllUser);
-
-router.route('/id/:id').get(userController.getUser);
+  .get(userController.getAllUser)
+  .post(userController.createUser);
+router
+  .route('/id/:id')
+  .get(userController.getUser)
+  .delete(userController.deleteUser)
+  .patch(userController.updateUser);
 
 module.exports = router;
